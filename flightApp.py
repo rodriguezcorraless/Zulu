0b373a1ff61ea6806634435628e449ed16a94010
sullivana6 <sullivana6@wit.edu>
2021-04-13 19:10:26 -0400

    implemented two way search

--- flightApp.py
+++ flightApp.py
@@ -22,20 +22,30 @@ def success(flight):
 def login():
     if request.method == 'POST':
         flightReq = request.form
-        flightDoc = flights.find({"dparture.iata": flightReq['origin'], "arrival.iata": flightReq['destination'],
-                                  "flight_date": flightReq["date"]})
-        flightData = [flight for flight in flightDoc]
-        df = format_dataframe(flightData)
+        if not flightReq['r_date']:
+            flightDoc = flights.find({"dparture.iata": flightReq['origin'], "arrival.iata": flightReq['destination'],
+                                      "flight_date": flightReq["d_date"]})
+            flightData = [flight for flight in flightDoc]
+            df = format_oneway(flightData)
+        else:
+            flightDoc1 = flights.find({"dparture.iata": flightReq['origin'], "arrival.iata": flightReq['destination'],
+                                      "flight_date": flightReq["d_date"]})
+            flightDoc2 = flights.find({"dparture.iata": flightReq['destination'], "arrival.iata": flightReq['origin'],
+                                      "flight_date": flightReq["r_date"]})
+            zipped_query = zip(flightDoc1, flightDoc2)
+            df = format_roundtrip(zipped_query)
 
         return render_template('zulu_response.html', tables=[flight.to_html(classes="data") for flight in df])
     else:
         return render_template('zulu.html')
 
 
-def format_dataframe(query):
+def format_oneway(query):
     labels = ["ID", "Price", "Airline", "Departure IATA", "Departure Airport", "Departure Gate", "Departure Time",
               "Arrival IATA",
               "Arrival Airport", "Arrival Gate", "Arrival Time"]
+    print(type(query))
+    print(query)
     frames = []
     for flight in query:
         data = [flight["flight"]["number"], flight["flight"]["price"], flight["airline"]["name"],
@@ -43,15 +53,30 @@ def format_dataframe(query):
                 flight["dparture"]["airport"], flight["dparture"]["gate"], flight["dparture"]["scheduled"],
                 flight["arrival"]["iata"], flight["arrival"]["airport"], flight["arrival"]["gate"],
                 flight["arrival"]["scheduled"]]
-        print(data)
         res = dict(zip(labels, data))
         df = pd.DataFrame(res, index=[0])
-        print(df)
         frames.append(df)
 
     return frames
 
 
+def format_roundtrip(query):
+    labels = ["ID", "Price", "Airline", "Departure IATA", "Departure Airport", "Departure Gate", "Departure Time",
+              "Arrival IATA",
+              "Arrival Airport", "Arrival Gate", "Arrival Time"]
+    frames = []
+
+    for flight1, flight2 in query:
+        data = [[flight1["flight"]["number"], flight2["flight"]["number"]], [flight1["flight"]["price"], flight2["flight"]["price"]], [flight1["airline"]["name"], flight2["airline"]["name"]],
+                [flight1["dparture"]["iata"], flight2["dparture"]["iata"]],
+                [flight1["dparture"]["airport"], flight2["dparture"]["airport"]], [flight1["dparture"]["gate"], flight2["dparture"]["gate"]], [flight1["dparture"]["scheduled"], flight2["dparture"]["scheduled"]],
+                [flight1["arrival"]["iata"], flight2["arrival"]["iata"]], [flight1["arrival"]["airport"], flight2["arrival"]["gate"]],
+                [flight1["arrival"]["scheduled"], flight2["arrival"]["scheduled"]]]
+        res = dict(zip(labels, data))
+        df = pd.DataFrame(res, index=[0, 1])
+        frames.append(df)
+
+    return frames
 
 
 if __name__ == '__main__':
--- main.py
+++ main.py
@@ -3,38 +3,38 @@ import datetime
 import pymongo
 import pandas as pd
 
-client = pymongo.MongoClient("mongodb+srv://Aidan:" + urllib.parse.quote_plus("#") + "@cluster0.uobls.mongodb.net/Flightdb?retryWrites=true&w=majority")
+client = pymongo.MongoClient("mongodb+srv://Aidan:" + urllib.parse.quote_plus("W@sab1dog") + "@cluster0.uobls.mongodb.net/Flightdb?retryWrites=true&w=majority")
 db = client.Flightdb
 flights = db.Flights
-flightDoc = {"flight_date": "2021-03-17", "flight_status": "scheduled",
-             "dparture":{"airport": "Adolfo Suárez Airport", "timezone": "Europe/Spain", "iata": "MAD", "icao": "KBOS",
-                          "terminal": "T3", "gate": "H22", "delay": None, "scheduled": "2021-03-14T10:00:00+00:00",
-                          "estimated": "2021-03-17T15:00:00+00:00", "actual": None, "estimated_runway": None,
+flightDoc = {"flight_date": "2021-04-21", "flight_status": "scheduled",
+             "dparture":{"airport": "Lelystad Airport", "timezone": "Europe/Spain", "iata": "AMS", "icao": "KBOS",
+                          "terminal": "T3", "gate": "H22", "delay": None, "scheduled": "2021-04-21T11:00:00+00:00",
+                          "estimated": "2021-04-21T15:00:00+00:00", "actual": None, "estimated_runway": None,
                           "actual_runway": None},
              "arrival": {"airport": "Logan International Airport", "timezone": "Europe\/Amsterdam", "iata": "BOS", "icao": "EHAM",
                          "terminal": "B", "gate": "B10", "baggage": None, "delay": None,
-                         "scheduled": "2021-03-17T12:07:00+00:00", "estimated": "2021-03-17T12:05:00+00:00",
+                         "scheduled": "2021-04-21T12:07:00+00:00", "estimated": "2021-04-21T12:07:00+00:00",
                          "actual": None, "estimated_runway": None, "actual_runway": None},
              "airline": {"name": "United Airlines", "iata": "UA", "icao": "DAL"},
-             "flight": {"number": "4153", "iata": "DL3097", "icao": "DAL3097", "price":"306",
+             "flight": {"number": "4153", "iata": "DL3097", "icao": "DAL3097", "price":"102",
                         "codeshared": {"airline_name": "uia", "airline_iata": "ps", "airline_icao": "aui",
                                        "flight_number": "101", "flight_iata": "ps101", "flight_icao": "aui101"}},
-             "aircraft": None, "live": None}, \
-            {"flight_date": "2021-03-17", "flight_status": "scheduled",
-             "dparture":{"airport": "Adolfo Suárez Airport", "timezone": "America/New_York", "iata": "MAD", "icao": "KBOS",
-                          "terminal": "T1", "gate": "H14", "delay": None, "scheduled": "2021-03-14T6:00:00+00:00",
-                          "estimated": "2021-03-17T10:00:00+00:00", "actual": None, "estimated_runway": None,
+             "aircraft": None, "live": None},\
+            {"flight_date": "2021-04-21", "flight_status": "scheduled",
+             "dparture":{"airport": "Lelystad Airport", "timezone": "Europe/Spain", "iata": "AMS", "icao": "KBOS",
+                          "terminal": "T3", "gate": "G19", "delay": None, "scheduled": "2021-04-21T10:00:00+00:00",
+                          "estimated": "2021-04-21T15:00:00+00:00", "actual": None, "estimated_runway": None,
                           "actual_runway": None},
              "arrival": {"airport": "Logan International Airport", "timezone": "Europe\/Amsterdam", "iata": "BOS", "icao": "EHAM",
-                         "terminal": "E", "gate": "E7", "baggage": None, "delay": None,
-                         "scheduled": "2021-03-17T12:14:00+00:00", "estimated": "2021-03-17T12:05:00+00:00",
+                         "terminal": "B", "gate": "B44", "baggage": None, "delay": None,
+                         "scheduled": "2021-04-21T12:07:00+00:00", "estimated": "2021-04-21T12:05:00+00:00",
                          "actual": None, "estimated_runway": None, "actual_runway": None},
-             "airline": {"name": "Delta", "iata": "DL", "icao": "DAL"},
-             "flight": {"number": "4179", "iata": "DL3097", "icao": "DAL3097", "price":"$479",
+             "airline": {"name": "United Airlines", "iata": "UA", "icao": "DAL"},
+             "flight": {"number": "4153", "iata": "DL3097", "icao": "DAL3097", "price":"102",
                         "codeshared": {"airline_name": "uia", "airline_iata": "ps", "airline_icao": "aui",
                                        "flight_number": "101", "flight_iata": "ps101", "flight_icao": "aui101"}},
              "aircraft": None, "live": None}
-#flights.insert_many(flightDoc)
+flights.insert_many(flightDoc)
 
 
 
