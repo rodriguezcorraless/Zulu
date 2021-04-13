3dec5e753eb92461c247dfb30c995c5823c83c2e
sullivana6 <sullivana6@wit.edu>
2021-04-12 22:18:46 -0400

    Fixed seperate dataframes for html interface

--- Scrape.py
+++ Scrape.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
 
 
 def expedia_scrape(origin, destination, depart_date):
@@ -15,4 +16,8 @@ def expedia_scrape(origin, destination, depart_date):
     return ids
 
 
-
+driver = webdriver.Chrome('C:/Users/sullivana6/PycharmProjects/Zulu/chromedriver')
+driver.get('https://www.orbitz.com/Flights-Search?flight-type=on&mode=search&trip=roundtrip&leg1=from%3ANew+York+%28NYC+-+All+Airports%29%2Cto%3AMiami+%28MIA+-+All+Airports%29%2Cdeparture%3A4%2F26%2F2021TANYT&options=cabinclass%3Aeconomy&leg2=from%3AMiami+%28MIA+-+All+Airports%29%2Cto%3ANew+York+%28NYC+-+All+Airports%29%2Cdeparture%3A4%2F27%2F2021TANYT&passengers=children%3A0%2Cadults%3A1%2Cseniors%3A0%2Cinfantinlap%3AY&fromDate=4%2F26%2F2021&toDate=4%2F27%2F2021&d1=2021-04-26&d2=2021-04-27')
+prices = driver.find_elements_by_xpath('//*[@id="flight-module-2021-04-26t06:30:00-04:00-coach-jfk-mia-aa-2268_2021-04-27t06:29:00-04:00-coach-mia-dca-aa-491-coach-dca-lga-aa-2117_"]/div[2]/div[2]/div[2]/div/div[1]/div[1]/span')[0]
+print(prices)
+print(type(prices))
--- flightApp.py
+++ flightApp.py
@@ -5,7 +5,8 @@ from flask import Flask, redirect, url_for, request, render_template
 from pymongo import MongoClient
 import pandas as pd
 
-client = pymongo.MongoClient("mongodb+srv://Aidan:" + urllib.parse.quote_plus("W@sab1dog") + "@cluster0.uobls.mongodb.net/Flightdb?retryWrites=true&w=majority")
+client = pymongo.MongoClient("mongodb+srv://Aidan:" + urllib.parse.quote_plus(
+    "W@sab1dog") + "@cluster0.uobls.mongodb.net/Flightdb?retryWrites=true&w=majority")
 db = client.Flightdb
 flights = db.Flights
 
@@ -26,24 +27,31 @@ def login():
         flightData = [flight for flight in flightDoc]
         df = format_dataframe(flightData)
 
-        return render_template('zulu_response.html', tables=[df.to_html(classes="data")])
+        return render_template('zulu_response.html', tables=[flight.to_html(classes="data") for flight in df])
     else:
         return render_template('zulu.html')
 
 
 def format_dataframe(query):
-    labels = ["ID", "Price", "Airline", "Departure IATA", "Departure Airport", "Departure Gate", "Departure Time", "Arrival IATA",
+    labels = ["ID", "Price", "Airline", "Departure IATA", "Departure Airport", "Departure Gate", "Departure Time",
+              "Arrival IATA",
               "Arrival Airport", "Arrival Gate", "Arrival Time"]
-    rows = []
+    frames = []
     for flight in query:
-        data = [flight["flight"]["number"], flight["flight"]["price"], flight["airline"]["name"], flight["dparture"]["iata"],
+        data = [flight["flight"]["number"], flight["flight"]["price"], flight["airline"]["name"],
+                flight["dparture"]["iata"],
                 flight["dparture"]["airport"], flight["dparture"]["gate"], flight["dparture"]["scheduled"],
                 flight["arrival"]["iata"], flight["arrival"]["airport"], flight["arrival"]["gate"],
                 flight["arrival"]["scheduled"]]
-        rows.append(data)
+        print(data)
+        res = dict(zip(labels, data))
+        df = pd.DataFrame(res, index=[0])
+        print(df)
+        frames.append(df)
 
-    df = pd.DataFrame(rows, columns=labels)
-    return df
+    return frames
+
+
 
 
 if __name__ == '__main__':
